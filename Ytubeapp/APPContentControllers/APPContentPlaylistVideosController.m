0f1cd935053383fc451319ad46468477433efa07
MStumpp <mstumpp@gmail.com>
2013-10-31 22:29:23 +0100

    playlist: replaced image with title

--- Ytubeapp/APPContentControllers/APPContentPlaylistVideosController.m
+++ Ytubeapp/APPContentControllers/APPContentPlaylistVideosController.m
@@ -29,7 +29,7 @@
     self = [super init];
     if (self) {
         self.playlist = pl;
-        self.topbarImage = [UIImage imageNamed:@"top_bar_back_playlists"];
+        self.topbarTitle = [[self.playlist title] stringValue];
         
         self.playlistVideosId = [NSString stringWithFormat:@"%@_%@", tPlaylistVideosAll, [APPContent playlistID:self.playlist]];
         
